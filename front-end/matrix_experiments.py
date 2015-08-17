4311484c91f25be491e7d3b9c9a3f9dda65bb576
asanka-code <asanka.code@gmail.com>
2015-08-17 16:09:54 +0530

    creating a list of links and generating the intial RSS vectors with dummy values

--- front-end/matrix_experiments.py
+++ front-end/matrix_experiments.py
@@ -5,49 +5,81 @@ import scipy.ndimage
 import matplotlib.pyplot as plt
 
 node_list = []
+links = []
 W = []
+Rs = []
 
 def addNodePosition(x, y):
 	"add the position of a sensor node"
 	node_list.append([x,y])
 	return
 
+def generateLinks():
+	i=0
+	while i<len(node_list):
+		j=i+1
+		# take the next node after the above selected node
+		while j < len(node_list):
+			links.append([ [ node_list[i][0],node_list[i][1] ], [ node_list[j][0], node_list[j][1] ] ])
+			j=j+1
+		i=i+1
+	i=0
 
 def generateWeightMatrix(width, height):
 	"generates the weight matrices for all the links"
-	# process each node in the node_list
 	i=0
-	while i<len(node_list):
-		j = i + 1
+	while i<len(links):
+		#print 'link',i,links[i][0], '-',links[i][1]
+		xpoint0, ypoint0 = links[i][0][0], links[i][0][1]
+		xpoint1, ypoint1 = links[i][1][0], links[i][1][1]
 
-		# take the next node after the above selected node
-		while j < len(node_list):
-			#print 'link=(', node_list[i][0], node_list[i][1], ') to (', node_list[j][0], node_list[j][1],')'
-			# Two points
-			xpoint0, ypoint0 = node_list[i][0], node_list[i][1]
-			xpoint1, ypoint1 = node_list[j][0], node_list[j][1]
+		# Extracting the coordinates of the voxels along the line
+		length = int(np.hypot(xpoint1-xpoint0, ypoint1-ypoint0)) + 1
+		xarray1, yarray1 = np.linspace(xpoint0, xpoint1, length), np.linspace(ypoint0, ypoint1, length)
 
-			# Extracting the coordinates of the voxels along the line
-			length = int(np.hypot(xpoint1-xpoint0, ypoint1-ypoint0)) + 1
-			xarray1, yarray1 = np.linspace(xpoint0, xpoint1, length), np.linspace(ypoint0, ypoint1, length)
+		# Generate the image matrix
+		z0 = np.random.uniform(0,0,size=(width,height))
 
-			# Generate the image matrix
-			z0 = np.random.uniform(0,0,size=(width,height))
+		# Don't delete the below line. I may need it oneday soon.
+		# Extract the values of the voxels along the line
+		#zi = z0[xarray1.astype(np.int), yarray1.astype(np.int)]
 
-			# Don't delete the below line. I may need it oneday soon.
-			# Extract the values of the voxels along the line
-			#zi = z0[xarray1.astype(np.int), yarray1.astype(np.int)]
+		# Set a new value to each voxel along the line
+		z0[xarray1.astype(np.int), yarray1.astype(np.int)] = 1
+		#print 'z0:'
+		#print z0
 
-			# Set a new value to each voxel along the line
-			z0[xarray1.astype(np.int), yarray1.astype(np.int)] = 1
-
-			#print 'z0:'
-			#print z0
-			
-			W.append(z0)
-
-			j = j + 1
+		W.append(z0)
 		i=i+1
+
+
+def genRSSCalibrationVector():
+	i=0
+	while i<len(links):
+		Rs.append(-18)
+		i=i+1
+	print 'Initial RSS vector Rs=',Rs	
+	return
+
+
+def getRSSVector():
+	R = []
+	i=0
+	while i<len(links):
+		R.append(-18)
+		i=i+1
+	print 'Current RSS vector R=',R
+	return R
+
+def getY():
+	Y = []
+	R = getRSSVector()
+	i=0
+	while i<len(links):
+		Y.append(Rs[i]-R[i])
+		i=i+1
+	return Y
+
 
 def initialize(width, height):
 	addNodePosition(0,0)
@@ -57,15 +89,10 @@ def initialize(width, height):
 	#print 'node_list='
 	#print node_list
 
+	generateLinks()
 	generateWeightMatrix(width, height)
-	'''
-	print 'len(W)=',len(W)
-	i=0
-	while i<len(W):
-		print 'W[',i,']:'
-		print W[i]
-		i = i + 1
-	'''
+	genRSSCalibrationVector()
+	print getY()
 	return W
 
 
